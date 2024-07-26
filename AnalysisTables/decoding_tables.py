a3a3189afd716d2adcafa65d1a93755fb8f65bc5
Sam Bray <sam.bray@ucsf.edu>
2024-07-26 13:18:34 -0700

    add table for storing ahead_behind distance (90s compute per epoch)

--- AnalysisTables/decoding_tables.py
+++ AnalysisTables/decoding_tables.py
@@ -1,8 +1,12 @@
 import datajoint as dj
+import pandas as pd
+
 from non_local_detector.visualization import create_interactive_1D_decoding_figurl
+from spyglass.common import AnalysisNwbfile
 from spyglass.decoding.v1.clusterless import ClusterlessDecodingV1
 from spyglass.decoding.v1.sorted_spikes import SortedSpikesDecodingV1
 from spyglass.utils import SpyglassMixin
+
 
 schema = dj.schema("ms_decoding")
 
@@ -53,3 +57,34 @@ class SortedSpikesDecodingFigurl_1D(SpyglassMixin, dj.Computed):
         )
         key["figurl"] = figurl
         self.insert1(key)
+
+
+@schema
+class ClusterlessAheadBehindDistance(SpyglassMixin, dj.Computed):
+    definition = """
+    -> ClusterlessDecodingV1
+    ---
+    -> AnalysisNwbfile
+    distance_object_id: varchar(128)
+    """
+
+    def make(self, key):
+        distance = (ClusterlessDecodingV1() & key).get_ahead_behind_distance()
+        results = (ClusterlessDecodingV1() & key).fetch_results()
+
+        df = pd.DataFrame({"time": results.time, "decode_distance": distance})
+        df = df.set_index("time")
+
+        analysis_file_name = AnalysisNwbfile().create(key["nwb_file_name"])
+        key["analysis_file_name"] = analysis_file_name
+        key["distance_object_id"] = AnalysisNwbfile().add_nwb_object(
+            analysis_file_name, df, "distance"
+        )
+        AnalysisNwbfile().add(key["nwb_file_name"], key["analysis_file_name"])
+
+        self.insert1(key)
+
+    def fetch1_dataframe(self) -> pd.DataFrame:
+        if not len(nwb := self.fetch_nwb()):
+            raise ValueError("fetch1_dataframe must be called on a single key")
+        return nwb[0]["distance"]
