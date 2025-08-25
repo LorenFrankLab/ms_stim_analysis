f29be62210d154aa7f8daa599fcb7e58e8165629
samuelbray32 <sam.bray@ucsf.edu>
2025-08-25 11:29:49 -0700

    remove unused utils and consolidate tables to folder

--- Utils/plot_helpers.py
+++ Utils/plot_helpers.py
@@ -2,20 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def format_ax(ax,
-              xlabel=None,
-              ylabel=None,
-              zlabel=None,
-              title=None,
-              title_color=None,
-              xlim=None,
-              ylim=None,
-              xticks=None,
-              xticklabels=None,
-              yticks=None,
-              yticklabels=None,
-              fontsize=20,
-              spines_off_list=["right", "top"]):
+def format_ax(
+    ax,
+    xlabel=None,
+    ylabel=None,
+    zlabel=None,
+    title=None,
+    title_color=None,
+    xlim=None,
+    ylim=None,
+    xticks=None,
+    xticklabels=None,
+    yticks=None,
+    yticklabels=None,
+    fontsize=20,
+    spines_off_list=["right", "top"],
+):
     """
     Format axis of plot.
     :param ax: axis object.
@@ -63,38 +65,3 @@ def format_ax(ax,
         ax.spines[spine].set_visible(False)
     # Axis
     ax.tick_params(labelsize=fontsize)
-
-
-def get_figsize(num_rows, num_columns, subplot_width, subplot_height):
-    return (subplot_width*num_columns, subplot_height*num_rows)
-
-
-def get_fig_axes(num_rows=None, num_columns=None, num_subplots=None, sharex=False, sharey=False, figsize=None,
-                 subplot_width=None, subplot_height=None, remove_empty_subplots=True, gridspec_kw=None):
-    # Check inputs not under or overspecified
-    if np.sum([x is None for x in [num_rows, num_columns, num_subplots]]) != 1:
-        raise Exception(f"Exactly two of num_rows, num_columns, num_subplots must be passed")
-    if figsize is not None and (subplot_width is not None or subplot_height is not None):
-        raise Exception(f"If figsize is passed, subplot_width and subplot_height should be None")
-
-    # Define missing parameters
-    if num_subplots is None:
-        num_subplots = num_rows*num_columns
-    if num_rows is None:
-        num_rows = int(np.ceil(num_subplots/num_columns))
-    elif num_columns is None:
-        num_columns = int(np.ceil(num_subplots/num_rows))
-
-    # If rows or columns is one, remove extra subplots if indicated
-    if remove_empty_subplots and (num_rows == 1 or num_columns == 1) and num_rows * num_columns < num_subplots:
-        if num_rows == 1:
-            num_columns = num_subplots
-        elif num_columns == 1:
-            num_rows = num_subplots
-
-    # Get figure size if not passed, using above params
-    if figsize is None and subplot_width is not None and subplot_height is not None:
-        figsize = get_figsize(num_rows, num_columns, subplot_width, subplot_height)
-
-    # Return subplots
-    return plt.subplots(num_rows, num_columns, sharex=sharex, sharey=sharey, figsize=figsize, gridspec_kw=gridspec_kw)
--- Utils/string_helpers.py
+++
@@ -1,6 +0,0 @@
-def snake_to_camel_case(string, capitalize_first=True):
-    split_string = string.split('_')
-    if capitalize_first:
-        return ''.join(x.title() for x in split_string)
-    return split_string[0] + ''.join(x.title() for x in split_string[1:])
-
